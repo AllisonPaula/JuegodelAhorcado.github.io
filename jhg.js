d15935b5c7074cf9de280154633a8c01c03a3ad3
Allison <paulaallison88@gmail.com>
2022-06-29 12:24:34 -0400

    Página en mantenimiento

--- jhg.js
+++
@@ -1,8 +0,0 @@
-const btnColores = document.querySelector("#colors");
-
-function colores(){
-    var array = ["ROJO", "AZUL", "AMARILLO", "TURQUEZA", "BEIGE", "ROSA", "NEGRO", "BLANCO", "MARRON", "VERDE", "NARANJA", "VIOLETA", "GRIS"];
-    var cambio = cambiar.replace("arr", "array");
-}
-
-btnColores.onclick = colores;
